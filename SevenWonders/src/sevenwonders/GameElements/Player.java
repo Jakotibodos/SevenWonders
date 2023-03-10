e80624fa227683b81de5ff5998e5ec8e21a80164
Jakotibodos <jakotibodos@gmail.com>
2023-03-10 16:38:54 +0100

    Started project

---
+++ SevenWonders/src/sevenwonders/GameElements/Player.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevenwonders.GameElements;
+
+/**
+ *
+ * @author jakot
+ */
+public class Player {
+    private Card[] hand;
+    private Card[] played;
+    private Wonder wonderBoard;
+    private int coins;
+    
+}
---
+++ SevenWonders/src/sevenwonders/SevenWonders.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sevenwonders;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import sevenwonders.GameElements.Card;
+import java.util.ArrayList;
+/**
+ *
+ * @author jakot
+ */
+public class SevenWonders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("Components/cards.csv");
+            Scanner myReader = new Scanner(myObj);
+            myReader.nextLine();
+
+            ArrayList<Card> deck = new ArrayList<>();
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] splitted = data.split(",");
+
+                int cardId = Integer.parseInt(splitted[0]);
+                String cardName = splitted[1];
+                String type = splitted[2];
+                String cost = splitted[3];
+                String provides = splitted[4];
+                String fFree = splitted[5];
+                String freeWith = splitted[6];
+                int age = Integer.parseInt(splitted[7]);
+
+                deck.add(new Card(cardId,cardName,type,cost,age));
+                
+            }
+            myReader.close();
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
